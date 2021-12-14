a8ef9d5531367e58c92e230bd5ab930790943f8b
Muhammad Mukhlish Saputra <muhammadmukhlish20@gmail.com>
2021-12-14 22:02:24 +0700

    update paragraf

--- MERN/my-react-app/src/App.js
+++ MERN/my-react-app/src/App.js
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
-          Something new happen
+          Hello World!!
         </p>
         <a
           className="App-link"
