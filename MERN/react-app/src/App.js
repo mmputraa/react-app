c9ade16ee2c8bed5c81c279d0c7e81ceb0912f6d
Muhammad Mukhlish Saputra <muhammadmukhlish20@gmail.com>
2021-12-17 19:25:55 +0700

    coba

--- MERN/react-app/src/App.js
+++ MERN/react-app/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Komponen from "./Materi/Komponen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React JS!
-          <br />
-          Semoga berhasil dan aman kedepannya
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
